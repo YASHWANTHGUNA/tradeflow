d897be2fcfa4c001d0139636a7e8394b8724d5e1
YASHWANTH <mail2yashwanth2004@gmail.com>
2026-06-13 21:56:41 +0530

    Fixed the issue of CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: ["https://tradeflow-peach.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Base endpoints mounted
